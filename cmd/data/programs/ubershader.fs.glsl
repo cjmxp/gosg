c0230ad7aaf4560078e23e3bed5dfb37a0d7226c
Filipe Varela <fcvarela@gmail.com>
2016-09-18 10:33:47 +0100

    Better BRDF

--- cmd/data/programs/ubershader.fs.glsl
+++ cmd/data/programs/ubershader.fs.glsl
@@ -99,71 +99,78 @@ vec3 tonemapUncharted2(vec3 color) {
     return curr * whiteScale;
 }
 
-/* BRDF */
-float fresnelSchlick( float f0, float dot_v_h ) {
-    return f0 + ( 1 - f0 ) * pow( 1.0 - dot_v_h, 5.0 );
+// beckmann
+float distribution(vec3 n, vec3 h, float roughness) {
+    float m_Sq = roughness * roughness;
+    float NdotH_Sq = max(dot(n, h), 0.0);
+    NdotH_Sq = NdotH_Sq * NdotH_Sq;
+    return exp((NdotH_Sq - 1.0) / (m_Sq*NdotH_Sq)) / (3.14159265 * m_Sq * NdotH_Sq * NdotH_Sq);
 }
 
-float fresnelSchlick( float dot_v_h )
-{
-    float value  = clamp( 1 - dot_v_h, 0.0, 1.0 );
-    float value2 = value * value;
-    return ( value2 * value2 * value );
+// cook-torrance
+float geometry(vec3 n, vec3 h, vec3 v, vec3 l, float roughness) {
+    float NdotH = dot(n, h);
+    float NdotL = dot(n, l);
+    float NdotV = dot(n, v);
+    float VdotH = dot(v, h);
+    float NdotL_clamped = max(NdotL, 0.0);
+    float NdotV_clamped = max(NdotV, 0.0);
+    return min(min(2.0 * NdotH * NdotV_clamped / VdotH, 2.0 * NdotH * NdotL_clamped / VdotH), 1.0);
 }
 
-float geomSchlickGGX( float alpha, float dot_n_v )
-{
-    float k    = 0.5 * alpha;
-    float geom = dot_n_v / ( dot_n_v * ( 1 - k ) + k );
-
-    return geom;
+// schlich
+float fresnel(float f0, vec3 n, vec3 l) {
+    return f0 + (1.0 - f0) * pow(1.0 - dot(n, l), 5.0);
 }
 
-float ndfGGX(float alpha, float NdotH )
-{
-    float alpha2 = alpha * alpha;
-    float t      = 1 + ( alpha2 - 1 ) * NdotH * NdotH;
-    return INV_PI * alpha2 / ( t * t );
-}
-
-vec3 BRDF(vec3 l, vec3 v, vec3 n, vec3 diffuse_color, float metalness, float roughness) {
-    float dot_n_v = dot(n, v);
-    float dot_n_l = dot(n, l);
-    float dot_n_l_clamp  = max(dot_n_l, 0.0);
-
-    vec3 h = normalize(l+v);
-    float dot_v_h = dot(v, h);
-    float fresnel = fresnelSchlick(dot_v_h);
-    vec3 f0_color = mix( diffuse_color, vec3(1.0), (1.0 - metalness));
-    vec3 specular_color = mix(f0_color, vec3(1.0), fresnel);
-
-    float clamp_roughness = max(roughness, 0.01);
-    float alpha = clamp_roughness * clamp_roughness;
-
-    float dot_n_h = dot(n, h);
-    float ndf = ndfGGX(alpha, dot_n_h);
-    float geom = geomSchlickGGX(alpha, dot_n_v);
-    float specular_brdf = (0.25 * ndf * geom ) / (dot_n_l * dot_n_v);
-    specular_color *= specular_brdf;
-
-    return dot_n_l_clamp * (INV_PI * (1.0 - metalness) * diffuse_color + specular_color);
+// fresnel diff
+float diffuse_energy_ratio(float f0, vec3 n, vec3 l) {
+    return 1.0 - fresnel(f0, n, l);
 }
 
 void main() {
-    vec3 N = normalize(tbn * (texture(normalTex, tcoords0.st).rgb * 2.0 - 1.0));
-    vec3 E = normalize(cameraPosition - position);
-    vec4 material = texture(diffuseTex, tcoords0.st);
-
-    int lc = int(lightCount[0]);
+    // init
     color.rgb = vec3(0.0);
 
+    // init materials
+    vec4 albedo = texture(diffuseTex, tcoords0.st);
+
+    // will come from textures later
+    float metalness = 0.0;
+    float roughness = 0.3;
+
+    // adjust f0 from 0.118 to 0.818, this will normally be discrete
+    float f0 = 0.118 + metalness * 0.7; //max 0.818
+
+    // normal, eye/view
+    vec3 N = normalize(tbn * (texture(normalTex, tcoords0.st).rgb * 2.0 - 1.0));
+    vec3 V = normalize(cameraPosition - position);
+
+    // shared products
+    float NdotV = dot(N, V);
+    float NdotV_clamped = max(NdotV, 0.0);
+
+    int lc = int(lightCount[0]);
     for (int i=0; i<lc; i++) {
-        vec3 lightPosition = lights[i].position.xyz;
-        vec3 L = normalize(lightPosition - position);
-        vec4 shadowLightPos = lights[i].vpMatrix * vec4(position, 1.0);
-        color.rgb += BRDF(L, E, N, material.rgb, 0.0, 0.7) * shadow(shadowLightPos, dot(N, L));
+        // lightdir, halfvec
+        vec3 L = normalize(lights[i].position.xyz - position);
+        vec3 H = normalize(L + V);
+
+        float NdotL = dot(N, L);
+        float NdotL_clamped = max(NdotL, 0.0);
+
+        float fres = fresnel(f0, H, L);
+        float geom = geometry(N, H, V, L, roughness);
+        float ndf = distribution(N, H,  roughness);
+
+        float brdf_spec = (0.25 * fres * geom * ndf) / (NdotL_clamped * NdotV_clamped);
+
+        vec3 color_spec = NdotL_clamped * brdf_spec * lights[i].diffuse.rgb;
+        vec3 color_diff = NdotL_clamped * diffuse_energy_ratio(f0, N, L) * albedo.rgb * lights[i].diffuse.rgb;
+        color.rgb += (color_diff + color_spec) * shadow(lights[i].vpMatrix * vec4(position, 1.0), dot(N, L));
     }
-    color.a = material.a;
+
+    color.a = albedo.a;
     color.rgb = tonemapUncharted2(color.rgb);
     color.rgb = pow(color.rgb, vec3(1.0/2.2));
 }
